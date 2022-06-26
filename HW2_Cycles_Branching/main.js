b5e7bb4a545a3cdab7621782e9fe61e80532a434
Chornyi Andrii <chornyy.andrey93@gmail.com>
2022-06-26 20:25:56 +0300

    Edited HW2

--- HW2_Cycles_Branching/main.js
+++ HW2_Cycles_Branching/main.js
@@ -3,45 +3,46 @@
 //1 - Отримайте у користувача число(N) від якого ми будемо складати. Отримати число можна за допомогою prompt. Пам'ятайте, ЧИСЛО, не РЯДОК!
 
 let N;
-    do {
-        N = +prompt ('Enter the number N');
-    } while (!Number.isInteger(N));
-console.log(`Enter the number N: ${N}`);
-document.writeln(`<b><br><br>Enter the number N: ${N}`);
+	do { 
+		N = +prompt ("Введіть число N:", '')
+	} while (!Number.isInteger(N));
+console.log(`Введіть число N: ${N}`);
+document.writeln(`<b><br><br> Введіть число N: ${N}`);
 
 //2 - Перевірте, чи є передане значення N ЦІЛИМ числом. (Підказка: якщо при конвертації в ціле число ми отримали NaN – це число нам не підходить)
 
-console.log('Чи ціле число N?',Number.isInteger(Number(N)));
+console.log("Чи ціле число N?", Number.isInteger(Number(N)));
 document.writeln(`<br><br>Іs this number an integer? ${Number.isInteger(Number(N))}`);
 
 //3 - Отримайте у користувача число(M) до якого ми будемо складати (включно). Отримати число можна за допомогою prompt. Пам'ятайте, ЧИСЛО, не РЯДОК!
 
 let M;
-do {
-    M = +prompt ('Enter the number M');
-} while (!Number.isInteger(M));
-console.log(`Enter the number M: ${M}`);
-document.writeln(`<br><br>Enter the number M: ${M}`);
+	do { 
+		M = +prompt ("Введіть число M:", '')
+	} while (!Number.isInteger(M));
+console.log(`Введіть число M: ${M}`);
+document.writeln(`<br><br> Введіть число M: ${M}`);
 
 //4 - Перевірте, чи є передане значення M ЦІЛИМ числом. (Підказка: якщо при конвертації в ціе число ми отримали NaN – це число нам не підходит)
 
-console.log('Чи ціле число M?',Number.isInteger(Number(M)));
+console.log("Чи ціле число M?", Number.isInteger(Number(M)));
 document.writeln(`<br><br>Іs this number an integer? ${Number.isInteger(Number(M))}`);
 
 //5 - Отримайте у користувача булевий параметр(true/false), який підкаже нам чи потрібно пропускати парні числа. TRUE – потрібно, FALSE – не потрібно. Використовуйте функцію confirm
 
-const evenNumber = confirm('Ignore even numbers?');
-console.log(`Ignore even numbers? ${evenNumber}`);
-document.writeln(`<br><br>Ignore even numbers? ${evenNumber}`);
+const skipEvenNum = confirm("Чи потрібно пропускати парні числа?")
+console.log(`Пропускати парні числа? ${skipEvenNum}`);
+document.writeln(`<br><br>Ignore even numbers? ${skipEvenNum}`);
 
 //6 - Напишіть цикл, який буде складати числа від раніше отриманих N и M
 
-let sumNumber = 0; 
-for (let i = N; i <= M; i++) {
-    if (evenNumber && i % 2 === 0) continue
-    sumNumber += i;
-}
+let sum = 0;
 
-console.log(`Sum of compound numbers: ${sumNumber}`)
-document.writeln(`<br><br>Sum of compound numbers: ${sumNumber}`)
+	for (let i = N; i <= M; i++) {
+		if (skipEvenNum && i % 2 === 0) continue
+	sum += i
+	};
+
+console.log(`Сума парних чисел: ${sum}`)
+document.writeln(`<br><br> Sum of compound numbers: ${sum}`)
 
--- HW5_Array/main.js
+++ HW5_Array/main.js
@@ -8,23 +8,18 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 // 1. Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. 
 
-// function pairBoysGirls (arr) {
+function pairs() {
 
-//     const boys
-//     const girls
-//     const boys
+    let arrForPairs = [];
+    
 
-// }
 
-function getPairs() {
-    const arrPairs = [];
-    let copyArr = [...STUDENTS];
-    copyArr.splice (1, 2, "Олена", "Ігор");
-    for (let i = 0; i < copyArr.length; i += 2) {
-        arrPairs.push([copyArr[i], copyArr[i + 1]]);
-    }
-    return arrPairs;
-};
+}
 
-console.log(arrPairs)
+
+
+
+
+
+
 
