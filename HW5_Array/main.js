7a6f29951932cc448799a79af8920cabfb371ead
Chornyi Andrii <chornyy.andrey93@gmail.com>
2022-06-30 18:48:54 +0300

    Edited & added new homework

--- HW5_Array/main.js
+++ HW5_Array/main.js
@@ -8,49 +8,39 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 // 1. Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. 
 
-	// створюємо копію масива students 
   
- 
-	
 let studentsDublicate = [...students]
 
 	let studentsPairs = []; 
   
   function getPairs() {
-  
-        // змінюємо позицію елементів за допомогою методу splice 
 
   	studentsDublicate.splice(1, 2, "Олена", "Ігор") 
       for (let i = 0; i < studentsDublicate.length; i += 2) {
         studentsPairs.push([studentsDublicate[i], studentsDublicate[i + 1]]);
       }
-      return studentsPairs;
-
+      
+  return studentsPairs;
   }
   
   const pairs = getPairs();
-
-console.log(pairs);
+  console.log(pairs);
 
 // 2. Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати.
 
-
 function getThemes() {
 
-let combineArr = [];
+  let combineArr = [];
 
-for (let i = 0; i < pairs.length; i++ ) {
-  combineArr.push([pairs[i].join(" i "), themes[i]])
-}
+    for (let i = 0; i < pairs.length; i++ ) {
+      combineArr.push([pairs[i].join(" i "), themes[i]])
+    }
 
-
-return combineArr;
-
-}
+  return combineArr;
+  }
 
 const getThemesPairs = getThemes();
-
-console.log(getThemes()); 
+console.log(getThemesPairs); 
 
 // 3.  Зіставте оцінки(marks) зі студентом(students)  
 
@@ -59,16 +49,31 @@ function getRating() {
 let ratingArr = [];
 
 	for (let i = 0; i < students.length; i++) {
-  
   ratingArr.push(students[i], marks[i]);
   }
 
-	return ratingArr;
+return ratingArr;
 }
 
 const rating = getRating();
-
 console.log(rating);
+
+// 4. Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт
+
+function getRandomRating () {
+
+  let randomRatingArr = []; 
+
+  for (let i = 0; getThemesPairs[i]; i++) {
+      let raindomRait = Math.floor(Math.random() * 5 + 1); 
+      randomRatingArr.push(getThemesPairs[i].slice());
+      randomRatingArr[i].push(raindomRait);
+  }
+  return randomRatingArr;
+}
+
+const randomRating = getRandomRating();
+console.log(randomRating);
 
 
 
