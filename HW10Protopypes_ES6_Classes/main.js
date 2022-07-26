45255834979f691db531dbb018aa2730b070d9dc
Chornyi Andrii <chornyy.andrey93@gmail.com>
2022-07-26 19:17:43 +0300

    Renamed folder for two HW and done HW10

--- HW10Protopypes_ES6_Classes/main.js
+++ HW10Protopypes_ES6_Classes/main.js
@@ -7,7 +7,10 @@ class Student {
     this.course = course;
     this.fullName = fullName;
     this.mark = mark;
+    this.dismissed = false;
   }
+
+  
 
 // 2. Cтворіть метод який повертає сукупну інформацію про курс, учбовий заклад та імені студента.
  
@@ -18,6 +21,10 @@ class Student {
 // 3. Створіть геттер оцінок this.marks, який повертає масив оцінок студента
   
   get studentMarks() {
+    if (this.dismissed == true) {
+      return null;
+    }
+  
   	return this.mark;
   }
 
@@ -33,8 +40,11 @@ set studentMarks(marks) {
      } else if (Number(marks) > 0 || Number(marks) < 6) {
         this.mark.push(Number(marks));
         console.log(randomStudent.studentMarks)
-
-      } else {
+      } 
+      else if (this.dismissed) {
+        return null;
+      }
+      else {
         console.log('Ви можете використовувати тільки оцінки 1-5, інші значення неможливі');
       }
   }
@@ -54,8 +64,15 @@ set studentMarks(marks) {
  Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. 
  (Ніяких помилок, просто повертається завжди null замість масиву оцінок) */
 
- 
+ dismiss() {
+  this.dismissed = true;
+    return `${this.fullName} був виключений.`
+ }
 
+ recover() {
+  this.dismissed = false;
+    return `${this.fullName} був поновлений`
+ }
 }
 
 const randomStudent = new Student('Міжнароднього Університету "Недостатньої Русофобії" м.Одеса', '1', 'Степан Бандера', [5, 4, 4, 5])
@@ -63,7 +80,42 @@ const randomStudent = new Student('Міжнароднього Університ
 console.log(randomStudent.info)
 console.log(randomStudent.studentMarks)
 randomStudent.studentMarks = 5;
-console.log(randomStudent.getAverageMark())
+console.log(randomStudent.getAverageMark());
+console.log(randomStudent.dismiss());
+console.log(randomStudent.studentMarks)
+console.log(randomStudent.recover());
+console.log(randomStudent.studentMarks)
+
+
+//  ---- ADVANCED  ----
+
+// 1. Створіть новий клас BudgetStudent, який повністю наслідує клас Student
+class BudgetStudent extends Student {
+  constructor(university, course, fullName, mark) {
+      super(university, course, fullName, mark)
+      //Метод отримання стипендії автоматично викликається кожні 30 секунд післе створення об'єкту
+        setInterval ( () => this.getScholarship(), 3000)
+  }
+
+/*Бюджетний студент може отримати стипендію за допомогою методу this.getScholarship. 
+Отримання стипендії супроводжується виведенням інформації в консоль: Ви отримали 1400 грн. стипендії */
+getScholarship() {
+  //Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0
+  if (!this.dismissed || this.getAverageMark() >= 4.0) {
+      return console.log('Ви отримали 1400 грн. стипендії')
+  } else {
+    return null;
+  }
+}
+
+}
+
+const newStudent = new BudgetStudent('Міжнароднього Університету "роzzійсько-Української війни" м.Київ', '1', 'Василь Стус', [5, 4, 4, 5])  
+
+console.log(newStudent.info)
+newStudent.getScholarship()
+
+
 
 
 
